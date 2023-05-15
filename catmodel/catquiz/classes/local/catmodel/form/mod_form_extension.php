a1d786340b33429301cff578f4f8f190319b9edd
David Szkiba <david.szkiba@wunderbyte.at>
2023-05-15 15:00:27 +0200

    Fix: implement abstract methods

--- catmodel/catquiz/classes/local/catmodel/form/mod_form_extension.php
+++ catmodel/catquiz/classes/local/catmodel/form/mod_form_extension.php
@@ -42,17 +42,10 @@ final class mod_form_extension implements
      * @param MoodleQuickForm $form
      */
     public function definition_callback(MoodleQuickForm $form): void {
+        $formelements = catquiz_handler::instance_form_definition($form);
 
-      //  $data = $form->exportValues();
-
-      //  if ($data['catmodel'] === 'catquiz') {
-            $formelements = catquiz_handler::instance_form_definition($form);
-
-            // At this point, we also apply the values we get from the template to the whole mform.
-
-            catquiz_handler::set_data_after_definition($form);
-
-       // }
+        // At this point, we also apply the values we get from the template to the whole mform.
+        catquiz_handler::set_data_after_definition($form);
     }
 
     /**
