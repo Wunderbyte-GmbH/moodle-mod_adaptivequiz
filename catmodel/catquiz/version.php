d30249ca74452f7a12ead6de1a0ddeed729bf6b7
David Szkiba <david.szkiba@wunderbyte.at>
2023-05-25 12:39:24 +0200

    Bump version number

--- catmodel/catquiz/version.php
+++ catmodel/catquiz/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023030700;
+$plugin->version = 2023052500;
 $plugin->release = '1.0.0';
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->requires = 2022041900;
