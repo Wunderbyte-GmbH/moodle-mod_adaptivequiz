097a8e03bdeb5838b9b812f21dd6873c22065d83
David Szkiba <david.szkiba@wunderbyte.at>
2024-03-14 11:26:21 +0100

    Bump version after removing helloworld

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024031400;
+$plugin->version = 2024031401;
 $plugin->release = '3.0.2dev';
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->requires = 2022041900;
